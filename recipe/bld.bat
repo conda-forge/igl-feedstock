0b0a000c1aaff01dd0e3ec9538e13251d0564790
Teseo Schneider <teseo@bluewin.ch>
2022-11-08 12:43:50 -0800

    cleaunp

--- recipe/bld.bat
+++
@@ -1,9 +0,0 @@
-REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
-REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
-REM set "CXXFLAGS=%CXXFLAGS:-GL=%"
-REM set "CFLAGS=%CFLAGS:-GL=%"
-set "CXXFLAGS= -MD"
-
-set "CMAKE_GENERATOR=Ninja"
-python --version
-python -m pip install . --no-deps -vv
